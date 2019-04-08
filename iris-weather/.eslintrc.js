e2f30a59500455866ef403b071b0d9fa5cefa370
leogordillo <leonel.gordillo37@gmail.com>
2019-04-08 06:32:54 -0500

    Added eslint

---
+++ iris-weather/.eslintrc.js
@@ -0,0 +1,43 @@
+// module.exports = {
+//     "env": {
+//         "es6": true,
+//         "node": true
+//     },
+//     "extends": "eslint:recommended",
+//     "globals": {
+//         "Atomics": "readonly",
+//         "SharedArrayBuffer": "readonly"
+//     },
+//     "parserOptions": {
+//         "ecmaVersion": 2018,
+//         "sourceType": "module"
+//     },
+//     "rules": {
+//     }
+// };
+
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
