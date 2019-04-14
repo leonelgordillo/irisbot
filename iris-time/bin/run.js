ae432c8a04987d04f8394c7717996ec7fa9234e7
leogordillo <leonel.gordillo37@gmail.com>
2019-04-14 16:15:19 -0500

    Added endpoint security to iris and iris-time

--- iris-time/bin/run.js
+++ iris-time/bin/run.js
@@ -9,21 +9,24 @@ const http = require('http');
 const server = http.createServer(service);
 server.listen();
 
-server.on('listening', function() {
+server.on('listening', function () {
     log.info(`IRIS-Time is listening on ${server.address().port} in ${service.get('env')} mode.`);
 
     const announce = () => {
-        request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`, (err, response) =>{
-            if(err){
-                log.debug(err);
-                log.info('Error connecting to Iris');
-                return;
-            }
-
-            log.info(response.body);
-        });
+        request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`)
+            .set('X-IRIS-SERVICE-TOKEN', config.serviceAccessToken)
+            .set('X-IRIS-API-TOKEN', config.irisApiToken)
+            .end((err, response) => {
+                if (err) {
+                    log.debug(err);
+                    log.info('Error connecting to Iris');
+                    return;
+                }
+                log.info('Response from announce: ');
+                log.info(response.body);
+            });
     };
 
     announce();
-    setInterval(announce, 15*1000);
+    setInterval(announce, 15 * 1000);
 });
