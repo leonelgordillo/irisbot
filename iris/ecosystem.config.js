a025bead73f55f4ef8e3d0436c1f2e54ee7c2380
leogordillo <leonel.gordillo37@gmail.com>
2019-11-16 08:41:26 -0600

    Adding pm2 files

---
+++ iris/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: 'IRIS',
+            script: 'bin/run.js',
+            env_production: {
+                NODE_ENV: 'production'
+            }
+        }
+    ],
+    deploy: {
+        production: {
+            user: 'node',
+            host: '<YOUR SERVER IP>',
+            ref: 'origin/master',
+            repo: 'https://github.com/leonelgordillo/irisbot.git',
+            path: '/srv/production',
+            'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production'
+        }
+    }
+};
